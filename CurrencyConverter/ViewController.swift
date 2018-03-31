4dea62f90593f877d66009f3caa20899ede6a437
milicakostic <mkostic078@gmail.com>
2018-03-31 21:47:00 +0200

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Milica Kostic on 3/31/18.
+//  Copyright © 2018 Milica Kostic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
